e64c640a9dd3695544eca5082572e241827e3223
Deysiane Lopes <deysilinconl@gmail.com>
2025-12-14 11:08:42 -0300

    initialize project structure with Spring Boot and Gradle setup

---
+++ src/main/java/com/deysi_lps/livrosshelf/LivrosShelfApplication.java
@@ -0,0 +1,13 @@
+package com.deysi_lps.livrosshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrosShelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivrosShelfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deysi_lps/livrosshelf/LivrosShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deysi_lps.livrosshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrosShelfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
